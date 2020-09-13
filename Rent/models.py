8569a16578960b04e2fc25ad010cf154ff436c0d
Hus782 <68049186+Hus782@users.noreply.github.com>
2020-09-13 19:28:03 +0300

    Update models.py

--- Rent/models.py
+++ Rent/models.py
@@ -2,31 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class MyUser(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=500, unique=True)
-    username = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    def __str__(self):
-        return self.username
-
-
-
 class Inventory(models.Model):
     name = models.CharField(max_length=255)
     status = models.CharField(max_length=255)
-    supplier = models.ForeignKey(MyUser, default=1, on_delete=models.CASCADE,)
+    supplier = models.ForeignKey(User, default=1, on_delete=models.CASCADE,)
 
     def __str__(self):
         return self.name
 
 
-class RentalRecord(models.Model):
-    student = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
-    item = models.ForeignKey(Inventory, default=1, on_delete=models.CASCADE,)
-    reason = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
-
+class RentApplication(models.Model):
+    student = models.ForeignKey(User,  on_delete=models.CASCADE, blank=True, null=True)
+    status = models.CharField(max_length=255,blank=True, null=True)
+    item = models.ForeignKey(Inventory,  on_delete=models.CASCADE, blank=True, null=True)
+    reason = models.CharField(max_length=255,blank=True, null=True)
+    date = models.DateField(auto_now_add=True, blank=True, null=True)
+    type = models.CharField(max_length=255,blank=True, null=True)
     def __str__(self):
-        return self.status
+        return self.type
+
+
+class SupplyAplication(models.Model):
+    student = models.ForeignKey(User, default=1, on_delete=models.CASCADE, )
+    status = models.CharField(max_length=255)
