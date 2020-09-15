ccbe103b2bc5ae30e7e1c603eed34a24f3594a57
Hus782 <68049186+Hus782@users.noreply.github.com>
2020-09-15 19:21:22 +0300

    Updated

--- Rent/urls.py
+++ Rent/urls.py
@@ -10,12 +10,14 @@ urlpatterns = [
         path('confirm/', views.register_confirm),
     ])),
     path('login/', views.my_login, name='login'),
-    path('', views.test, name='home'),      #tempoary modified for test
     path('logout/', views.mylogout, name='logout'),
-    path('apply/', views.apply_for_provider, name='apply'),
-
-    path('supplier/',views.apply_for_provider,name='apply_supplier'),
-    path('rented/<int:user_id>',views.check_rented_items,name='check_rented_items'),
-    path('<int:user_id>/<str:request_type>',views.check_all_requests,name='check_requests'),
-    path('<int:user_id>/',views.user_info,name='user_info'),
+    path('user/supplier', views.apply_for_supplier, name='apply'),
+    path('rent/<int:item_id>', views.apply_for_item, name='rent'),
+    path('supplier/add/', views.apply_add_item, name='rent'),
+    path('supplier/<int:user_id>/edit/<int:item_id>', views.edit_item, name='rent'),
+    path('supplier/<int:user_id>/return/<int:item_id>', views.return_rented_item, name='rent'),
+    path('<int:user_id>/check/<int:req_id>/<str:action>', views.check_item_application, name='rent'),
+    path('rented/<int:user_id>', views.check_rented_items, name='check_rented_items'),
+    path('<int:user_id>/requests/<str:request_type>', views.check_all_requests, name='check_requests'),
+    path('<int:user_id>/info', views.user_info, name='user_info'),
 ]
