bda4ddc26bfebcf718640bff8326b9b0e67c6b5a
CHARLES_QIN <42432987+1723225155@users.noreply.github.com>
2020-09-16 00:21:12 +0800

    Update forms.py

--- Rent/forms.py
+++ Rent/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.db import models
+from .models import EmailValid
 from django.contrib.auth.forms import UserCreationForm
 
 passwordInputWidget = {
     'password': forms.PasswordInput(),
 }
+
+
+class EmailForm(forms.ModelForm):
+    class Meta:
+        model = EmailValid
+        fields = ['email', 'sec_code']
 
 
 class RegisterForm(forms.ModelForm):
