0b502decb4747d5c85bc3b6b2a70c25e9a4dad2b
CHARLES_QIN <42432987+1723225155@users.noreply.github.com>
2020-09-16 00:20:01 +0800

    Update urls.py

--- Rent/urls.py
+++ Rent/urls.py
@@ -1,14 +1,19 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 app_name = 'Rent'
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    path('register/', include([
+        path('', views.register_page),
+        path('page/', views.register_page),
+        path('sendCode/', views.register_get_code),
+        path('confirm/', views.register_confirm),
+    ])),
     path('login/', views.my_login, name='login'),
     path('', views.test, name='home'),      #tempoary modified for test
     path('logout/', views.mylogout, name='logout'),
     path('apply/', views.apply_for_provider, name='apply'),
-    
+
     path('supplier/',views.apply_for_provider,name='apply_supplier'),
     path('rented/<int:user_id>',views.check_rented_items,name='check_rented_items'),
     path('<int:user_id>/<str:request_type>',views.check_all_requests,name='check_requests'),
